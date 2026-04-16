3a91cab2f6786e53ae56317e6a3358023e92fc4b
SakoPolis <40133288+SakoPolis@users.noreply.github.com>
2026-04-16 00:37:47 -0700

    stylesheet updated, added distance to dashboard, added call button, replaced pet image with my own pet image

--- script.js
+++ script.js
@@ -3,26 +3,44 @@ const petInfo = {
     name: "Discord Kitten",
     weight: 12,
     happiness: 8,
+    distance: 10,
 };
 $(function () {
     checkAndUpdatePetInfoInHtml();
     $(".treat-button").on("click", clickedTreatButton);
     $(".play-button").on("click", clickedPlayButton);
     $(".exercise-button").on("click", clickedExerciseButton);
+    $(".call-button").on("click", clickedCallButton);
 });
+function animateButton(buttonSelector) {
+    const $button = $(buttonSelector);
+    $button.addClass("animating");
+    setTimeout(() => {
+        $button.removeClass("animating");
+    }, 600);
+}
 function clickedTreatButton() {
+    animateButton(".treat-button");
     petInfo.happiness += 1;
     petInfo.weight += 1;
     checkAndUpdatePetInfoInHtml();
 }
 function clickedPlayButton() {
+    animateButton(".play-button");
     petInfo.happiness += 2;
     petInfo.weight -= 1;
     checkAndUpdatePetInfoInHtml();
 }
 function clickedExerciseButton() {
+    animateButton(".exercise-button");
     petInfo.happiness -= 1;
     petInfo.weight -= 2;
+    checkAndUpdatePetInfoInHtml();
+}
+function clickedCallButton() {
+    animateButton(".call-button");
+    petInfo.happiness += 1;
+    petInfo.distance -= 2;
     checkAndUpdatePetInfoInHtml();
 }
 function checkAndUpdatePetInfoInHtml() {
@@ -36,10 +54,14 @@ function checkWeightAndHappinessBeforeUpdating() {
     if (petInfo.happiness < 0) {
         petInfo.happiness = 0;
     }
+    if (petInfo.distance < 0) {
+        petInfo.distance = 0;
+    }
 }
 function updatePetInfoInHtml() {
     $(".name").text(petInfo.name);
     $(".weight").text(petInfo.weight);
     $(".happiness").text(petInfo.happiness);
+    $(".distance").text(petInfo.distance);
 }
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -2,12 +2,14 @@ interface PetInfo {
   name: string;
   weight: number;
   happiness: number;
+  distance: number;
 }
 
 const petInfo: PetInfo = {
   name: "Discord Kitten",
   weight: 12,
   happiness: 8,
+  distance: 10,
 };
 
 $(function (): void {
@@ -16,23 +18,42 @@ $(function (): void {
   $(".treat-button").on("click", clickedTreatButton);
   $(".play-button").on("click", clickedPlayButton);
   $(".exercise-button").on("click", clickedExerciseButton);
+  $(".call-button").on("click", clickedCallButton);
 });
 
+function animateButton(buttonSelector: string): void {
+  const $button = $(buttonSelector);
+  $button.addClass("animating");
+  setTimeout(() => {
+    $button.removeClass("animating");
+  }, 600);
+}
+
 function clickedTreatButton(): void {
+  animateButton(".treat-button");
   petInfo.happiness += 1;
   petInfo.weight += 1;
   checkAndUpdatePetInfoInHtml();
 }
 
 function clickedPlayButton(): void {
+  animateButton(".play-button");
   petInfo.happiness += 2;
   petInfo.weight -= 1;
   checkAndUpdatePetInfoInHtml();
 }
 
 function clickedExerciseButton(): void {
+  animateButton(".exercise-button");
   petInfo.happiness -= 1;
   petInfo.weight -= 2;
+  checkAndUpdatePetInfoInHtml();
+}
+
+function clickedCallButton(): void {
+  animateButton(".call-button");
+  petInfo.happiness += 1;
+  petInfo.distance -= 2;
   checkAndUpdatePetInfoInHtml();
 }
 
@@ -49,10 +70,15 @@ function checkWeightAndHappinessBeforeUpdating(): void {
   if (petInfo.happiness < 0) {
     petInfo.happiness = 0;
   }
+
+  if (petInfo.distance < 0) {
+    petInfo.distance = 0;
+  }
 }
 
 function updatePetInfoInHtml(): void {
   $(".name").text(petInfo.name);
   $(".weight").text(petInfo.weight);
   $(".happiness").text(petInfo.happiness);
+  $(".distance").text(petInfo.distance);
 }
